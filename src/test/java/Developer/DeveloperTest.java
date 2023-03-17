bc487b55d31528f04f706fb868aaa4a5b8b22833
Markony994 <markonovakovic994@gmail.com>
2023-03-17 15:36:33 +0100

    Task 5

--- src/test/java/Developer/DeveloperTest.java
+++ src/test/java/Developer/DeveloperTest.java
@@ -4,49 +4,46 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import tech.enfint.AppConfig;
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
 import tech.enfint.Developer;
 import tech.enfint.Log;
+import tech.enfint.Manager;
 
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 public class DeveloperTest
 {
     @InjectMocks
     private Developer victim;
-    Log log = new Log();
-    @Mock
-    ProceedingJoinPoint proceedingJoinPoint;
-    private ConfigurableApplicationContext context;
+    @Spy
+    Log log;
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static AutoCloseable closeable;
 
     @Before
     public void setUp()
     {
-        // Configure the bean to be added to application context and set mock dependencies, to avoid bean initialization exception.
-        final Developer developer = new Developer();
         closeable = openMocks(this);
-        //set here other dependencies on victim if required
-        context = new AnnotationConfigApplicationContext(AppConfig.class);
-        final AutowireCapableBeanFactory autowireFactory = context.getAutowireCapableBeanFactory();
-        victim = (Developer) autowireFactory.initializeBean(developer, "factoryDev");
+        AspectJProxyFactory factory = new AspectJProxyFactory(new Developer(new Manager()));
+        factory.addAspect(log);
+        victim = factory.getProxy();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        reset(log);
     }
 
     @After
     public void tearDown() throws Exception {
-        context.close();
         closeable.close();
     }
 
@@ -56,23 +53,23 @@ public class DeveloperTest
         assertThrows(ArithmeticException.class, () ->
         {
             victim.divide(10, 0);
-            verify(proceedingJoinPoint, times(1)).proceed();
         });
+
+        verify(log, times(1)).logAround(any(ProceedingJoinPoint.class));
     }
 
     @Test
     public void testDivideBiggerWithSmaller() throws Throwable
     {
         assertEquals(2, victim.divide(10, 5));
-
-        verify(proceedingJoinPoint, times(1)).proceed();
+        verify(log, times(1)).logAround(any(ProceedingJoinPoint.class));
     }
 
     @Test
     public void testDivideSmallerWithBigger() throws Throwable
     {
         assertEquals(0, victim.divide(5, 10));
-        verify(proceedingJoinPoint, times(1)).proceed();
+        verify(log, times(1)).logAround(any(ProceedingJoinPoint.class));
     }
 
 }//public class Developer.DeveloperClass
